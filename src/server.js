f8d80d106e6125e49a44f83e7281aafb4b81e241
gabe <gabriel.smith01@gmail.com>
2017-06-23 21:44:36 -0400

    code cleanup

--- src/server.js
+++ src/server.js
@@ -39,19 +39,6 @@ passport.use(new JwtStrategy(jwtOptions, (jwt_payload, done) => {
     }
   })
 }))
-// passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-//   User.findOne({ email: email.toLowerCase() }, (err, user) => {
-//     if (err) { return done(err); }
-//     if (!user) {
-//       return done(null, false, { 'message': `Email ${email} not found.` });
-//     }
-//     if (!user.validPassword(password)) {
-//       return done(null, false, { 'message': 'Incorrect password.' });
-//     }
-//     return done(null, user);
-//   });
-// }));
-
 
 const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
